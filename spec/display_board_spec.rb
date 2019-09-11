40b7c31e7868ba7053d94409c7c03911b3b8dd6b
Neil Ryan <neilmarkoliverryan@gmail.com>
2019-09-11 18:54:12 +0000

    Automatically backed up by Learn

--- spec/display_board_spec.rb
+++ spec/display_board_spec.rb
@@ -178,13 +178,13 @@ describe "#display_board in 'lib/display_board.rb" do
     it 'prints an entire board full of Os' do
       # Can you copy the syntax of the tests above to write a test for a board
       # entirely filled with Os?
-
+      board = ["X", "X", "X", "X", "X", "X", "X", "X", "X"]
       # Hint: You should be able to copy the code in the previous it example
       # and make a few simple edits to convert the previous example to this
       # example's situation.
 
       # *** Comment the line below by adding a # at the line start ***
-      expect(true).to be(true)
+      #expect(true).to be(true)
     end
   end
 end
